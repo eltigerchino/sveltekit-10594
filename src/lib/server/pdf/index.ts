703234ac9a311879a58e403af5933434127aaec1
Tee Ming <chewteeming01@gmail.com>
2023-09-21 07:11:38 +0800

    add netlify server path

--- src/lib/server/pdf/index.ts
+++ src/lib/server/pdf/index.ts
@@ -4,49 +4,51 @@ import type { TDocumentDefinitions, TFontDictionary } from 'pdfmake/interfaces';
 import futuraNormal from '$lib/fonts/FuturaPTBook.otf';
 import futuraBold from '$lib/fonts/FuturaPTCondBold.otf';
 import path from 'path';
+import { building } from '$app/environment';
 // import { fileURLToPath } from 'url';
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
+let dir = process.cwd();
+if (building) {
+	dir += '/.netlify/server';
+}
+
 const fonts: TFontDictionary = {
 	Futura: {
-		normal: path.join(process.cwd(), futuraNormal),
-		bold: path.join(process.cwd(), futuraBold)
+		normal: path.join(dir, futuraNormal),
+		bold: path.join(dir, futuraBold)
 	}
 };
 
 const printer = new PdfPrinter(fonts);
 
 async function genInvoicePdf(): Promise<Blob> {
+	const file: TDocumentDefinitions = {
+		pageMargins: [40, 40, 40, 60],
+		content: ['Hello, World!!'],
+		defaultStyle: {
+			font: 'Futura'
+		}
+	};
 
-    const file: TDocumentDefinitions = {
+	return new Promise((resolve, reject) => {
+		const pdf = printer.createPdfKitDocument(file);
 
-        pageMargins: [40, 40, 40, 60],
-        content: [
-            "Hello, World!!"
-        ],
-        defaultStyle: {
-            font: 'Futura'
-        }
-    };
+		pdf
+			.pipe(blobStream())
+			.on('finish', function (this: IBlobStream) {
+				console.log('Finished generating PDF');
+				resolve(this.toBlob('application/pdf'));
+			})
+			.on('error', (err) => {
+				console.error('err', err);
+				reject(err);
+			});
 
-    return new Promise((resolve, reject) => {
-        const pdf = printer.createPdfKitDocument(file);
-
-        pdf
-            .pipe(blobStream())
-            .on('finish', function (this: IBlobStream) {
-                console.log('Finished generating PDF');
-                resolve(this.toBlob('application/pdf'));
-            })
-            .on('error', (err) => {
-                console.error('err', err);
-                reject(err);
-            });
-
-        pdf.end();
-    });
+		pdf.end();
+	});
 }
 
 export default genInvoicePdf;
